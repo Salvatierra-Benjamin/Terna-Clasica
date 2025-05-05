820563820af4636e1696a8b050ae38156ab42243
Benjamin Salvatierra <bsalvatierratola@frba.utn.edu.ar>
2025-05-05 02:02:08 -0300

    implementación js

--- Proyecto/codigo/main.js
+++ Proyecto/codigo/main.js
@@ -1,1 +1,26 @@
-// inicio
+console.log("hola");
+const pantalla = document.getElementById('pantalla');
+const contenedor = document.getElementById('botones');
+const botones = contenedor.querySelectorAll('button');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const botonApretado = boton.textContent;
+
+        if (boton.id === "del") {
+            pantalla.textContent = '0';
+            return;
+        }
+
+        if (boton.id === "borrar") {
+            pantalla.textcontent = pantalla.textContent.slice(0, -1);
+            return;
+        }
+
+        if (pantalla.textContent === '0') {
+            pantalla.textContent = botonApretado;
+        } else {
+            pantalla.textContent += pantalla.textContent;
+        }
+    });
+});
